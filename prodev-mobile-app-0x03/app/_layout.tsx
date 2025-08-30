be5b6e2f320b1215ce1f68f10e9496b479c2da20
odusdegreat <tobiodubote@gmail.com>
2025-08-30 14:35:04 +0100

    fix: fixed some errors

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{}} />;
+  return (
+    <Stack
+      screenOptions={{
+       [ "headerShown: false"]
+      }}
+    />
+  );
 }
